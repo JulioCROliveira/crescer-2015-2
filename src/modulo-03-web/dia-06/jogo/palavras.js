33c37c5147b2e924536f22ff8da7a5064d05c782
Júlio César Rodrigues de Oliveira <julio31p@hotmail.com>
2015-10-24 11:53:29 -0200

    feat(jogo-da-forca) adicionado armazenamento em localStorage.

--- src/modulo-03-web/dia-06/jogo/palavras.js
+++ src/modulo-03-web/dia-06/jogo/palavras.js
@@ -3,20 +3,41 @@ function getPalavra(dificultade) {
 	var indicePalavrasPorDificultade = 2;
 	var indiceDaPalavra = 0;
 	var palavraDeRetorno = {};
+	if (localStorage.palavraRepetidas === undefined) { 
+		resetaPalavrasRepetidas();
+	}
 	if (dificultade === 'normal') {
 		indicePalavrasPorDificultade = Math.random() < 0.49 ? 1 : 2;
 	}
-	$.get('http://localhost:3000/palavras?idDificuldade='+indicePalavrasPorDificultade).done(function(palavras) {
-		var indiceDaPalavra = Math.floor(Math.random() * palavras.length);
-		palavraDeRetorno = palavras[indiceDaPalavra];
+	$.get.call(this,'http://localhost:3000/palavras?idDificuldade='+indicePalavrasPorDificultade).done(function(palavras) {
+		var palavrasRepetidas = JSON.parse(localStorage.palavraRepetidas);
+		do {
+			var indiceDaPalavra = Math.floor(Math.random() * palavras.length);
+			palavraDeRetorno = palavras[indiceDaPalavra];
+		} while (containsPalavra(palavrasRepetidas, palavraDeRetorno));
+		palavrasRepetidas.push(palavraDeRetorno);
+		localStorage.setItem('palavraRepetidas', JSON.stringify(palavrasRepetidas));
 		console.log(palavraDeRetorno); // não retorna pelo return
 	});	
 	
 	return palavraDeRetorno;
 };
 
+function containsPalavra(arr, palavras) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].palavra === palavras.palavra) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function resetaPalavrasRepetidas() {
+	localStorage.setItem('palavraRepetidas', '[]');
+}
+
 function adicionarPalavra(palavra, dica) {
 	indicePalavrasPorDificultade = palavra.length < 12 ? 1 : 2;
 	$.post('http://localhost:3000/palavras?idDificuldade=', 
-	{"palavra": palavra, "dica": dica, 	"idDificuldade": indicePalavrasPorDificultade});	
+	{"palavra": palavra.toUpperCase(), "dica": dica, 	"idDificuldade": indicePalavrasPorDificultade});	
 };
