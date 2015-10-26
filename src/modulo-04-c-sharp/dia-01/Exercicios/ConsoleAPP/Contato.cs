a9b90a6b4c15678920c501d6bac9201a9de51a6f
Júlio César Rodrigues de Oliveira <julio31p@hotmail.com>
2015-10-26 17:13:34 -0200

    feat(AgendaConta): adicionar classes Agenda e Contato

---
+++ src/modulo-04-c-sharp/dia-01/Exercicios/ConsoleAPP/Contato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAPP
+{
+    public class Contato
+    {
+        public string nome;
+        public int numero { get; set; }
+
+        public Contato(string nome, int numero)
+        {
+            this.nome = nome;
+            this.numero = numero;
+        }
+    }
+}
---
+++ src/modulo-04-c-sharp/dia-01/Exercicios/ConsoleAPP/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAPP
+{
+    class Program
+    {
+        private static Agenda agenda = new Agenda();
+
+        static void Main(string[] args)
+        {
+            int opcao;
+            do
+            {
+                Console.WriteLine("Digite 1 para cadastrar novo contato\r\nDigite 2 para visualizar contatos\r\nDigite 0 para sair");
+                opcao = int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        CadastrarContatoNovo();
+                        Console.WriteLine("Contato adicionado.");
+                        break;
+                    case 2:
+                        Console.WriteLine(listarContatos());
+                        break;
+                    case 0:
+                        Console.WriteLine("Saindo...");
+                        break;
+                    default:
+                        Console.WriteLine("opção invalida!");                        
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+            while (opcao != 0);
+        }
+
+        public static void CadastrarContatoNovo()
+        {
+            string nome;
+            int numero;
+            Console.WriteLine("Escreva o nome do contato :");
+            nome = Console.ReadLine();
+            Console.WriteLine("Escreva o numero do contato :");
+            numero = int.Parse(Console.ReadLine());
+            agenda.AdicionarContato(new Contato(nome, numero));
+        }
+
+        public static string listarContatos()
+        {
+            return agenda.ListarContatosOrdenadoPorNome();
+        }
+    }
+}
