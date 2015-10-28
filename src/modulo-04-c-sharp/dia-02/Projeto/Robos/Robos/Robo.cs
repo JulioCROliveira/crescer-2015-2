a2e82d438186081a035339b4b60e396aa3bd09eb
Júlio César Rodrigues de Oliveira <julio31p@hotmail.com>
2015-10-28 15:35:17 -0200

    commit

---
+++ src/modulo-04-c-sharp/dia-02/Projeto/Robos/Robos/Robo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robos
+{
+    public abstract class Robo
+    {
+        public string Nome { get; private set; }
+
+        public Robo(string nome)
+        {
+            nome = nome;
+        }
+
+        public virtual void Atacar()
+        {
+            Console.WriteLine("Atacar");
+        }
+    }    
+}
