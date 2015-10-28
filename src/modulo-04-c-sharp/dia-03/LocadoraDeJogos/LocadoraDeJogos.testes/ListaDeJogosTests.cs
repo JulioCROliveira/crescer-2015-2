c5304c0eedabf101f939e6d38d4b28d51ed43b52
Júlio César Rodrigues de Oliveira <julio31p@hotmail.com>
2015-10-28 17:35:36 -0200

    feat(LocadoraDeJogos): iniciando a modelagem;

---
+++ src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/LocadoraDeJogos.testes/ListaDeJogosTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocadoraDeJogos.Dominio;
+
+namespace LocadoraDeJogos.testes
+{
+    [TestClass]
+    public class ListaDeJogosTests
+    {
+        [TestMethod]
+        public void listarJogos()
+        {
+            ListaDeJogos.listarJogos();
+        }
+    }
+}
