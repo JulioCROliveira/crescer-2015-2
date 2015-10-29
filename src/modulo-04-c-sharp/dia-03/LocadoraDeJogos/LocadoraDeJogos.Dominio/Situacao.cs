7808ebe95957d1ba4406ab923fd77f744f3c1cdb
Júlio César Rodrigues de Oliveira <julio31p@hotmail.com>
2015-10-29 18:46:22 -0200

    feat(LocadoraDeJogos): adicionar projetos

---
+++ src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/LocadoraDeJogos.Dominio/Situacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeJogos.Dominio
+{
+    public enum Situacao
+    {
+        Disponivel = 0,
+        Indisponivel = 1,
+        Inativo = 2
+    }
+}
---
+++ src/modulo-04-c-sharp/dia-03/LocadoraDeJogos/LocadoraDeJogos.IO/Program.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraDeJogos.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeJogos.IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lista = ListaDeJogos.listarJogos();
+
+            Console.WriteLine(lista);
+            Console.ReadKey();
+        }
+    }
+}
