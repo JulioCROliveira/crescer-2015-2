2779d97c030386a1b366a68ebe7535f29d78433c
Júlio César Rodrigues de Oliveira <julio31p@hotmail.com>
2015-11-12 22:05:08 -0200

    feat(Locadora): adicionar LocacaoRepositorio

---
+++ src/modulo-04-c-sharp/LocadoraDeJogos/Locadora.Dominio/Repositorio/IRepositorioLocacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio.Repositorio
+{
+    public interface IRepositorioLocacao
+    {
+        IList<Locacao> BuscarPorJogo(Jogo jogo);
+        IList<Locacao> BuscarTodos();
+        int Criar(Locacao locacao);
+        int DevolverJogo(Locacao locacao);
+        Locacao BuscarPorId(int id);
+        IList<Locacao> BuscarAtivos();
+    }
+}
---
+++ src/modulo-04-c-sharp/LocadoraDeJogos/Locadora.Repositorio.EF/Repositorios/LocacaoRepositorio.cs
@@ -0,0 +1,64 @@
+﻿using Locadora.Dominio;
+using Locadora.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Repositorio.EF.Repositorios
+{
+    public class LocacaoRepositorio : IRepositorioLocacao
+    {
+        public IList<Locacao> BuscarPorJogo(Jogo jogo)
+        {
+            using (BancoDeDados db = new BancoDeDados())
+            {
+                return db.Locacao.Include("Jogo").Include("Cliente").Where(l => l.IdJogo == jogo.Id).ToList();
+            } 
+        }
+
+        public IList<Locacao> BuscarTodos()
+        {
+            using (BancoDeDados db = new BancoDeDados())
+            {
+                return db.Locacao.Include("Jogo").Include("Cliente").ToList();
+            } 
+        }
+
+        public int Criar(Locacao locacao)
+        {
+            using (BancoDeDados db = new BancoDeDados())
+            {
+                db.Entry(locacao).State = System.Data.Entity.EntityState.Added;
+                return db.SaveChanges();
+            } 
+        }
+
+        public int DevolverJogo(Locacao locacao)
+        {
+            using (BancoDeDados db = new BancoDeDados())
+            {
+                locacao.Situacao = Situacao.ENTREGUE;
+                db.Entry(locacao).Property(p => p.Situacao).IsModified = true;
+                return db.SaveChanges();
+            } 
+        }
+
+        public Locacao BuscarPorId(int id)
+        {
+            using (BancoDeDados db = new BancoDeDados())
+            {
+                return db.Locacao.Include("Jogo").Include("Cliente").FirstOrDefault(p => p.Id == id);
+            } 
+        }
+
+        public IList<Locacao> BuscarAtivos()
+        {
+            using (BancoDeDados db = new BancoDeDados())
+            {
+                return db.Locacao.Include("Jogo").Include("Cliente").Where(p => p.Situacao == Situacao.ATIVO).ToList();
+            } 
+        }
+    }
+}
