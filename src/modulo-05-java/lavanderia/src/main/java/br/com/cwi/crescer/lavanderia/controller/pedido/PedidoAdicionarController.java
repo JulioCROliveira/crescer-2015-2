2d5d4cf263ed03bb9b352dee08d801bfa6d805a4
Júlio César Rodrigues de Oliveira <julio31p@hotmail.com>
2015-12-01 16:52:26 -0200

    listar clientes para pedido

--- src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/controller/pedido/PedidoAdicionarController.java
+++ src/modulo-05-java/lavanderia/src/main/java/br/com/cwi/crescer/lavanderia/controller/pedido/PedidoAdicionarController.java
@@ -4,10 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.cwi.crescer.lavanderia.dto.pedido.PedidoDTO;
 import br.com.cwi.crescer.lavanderia.services.ClienteService;
 import br.com.cwi.crescer.lavanderia.services.PedidoService;
 
@@ -30,5 +32,13 @@ public class PedidoAdicionarController extends PedidoController {
             return new ModelAndView("pedido/listaClientes", "clientes", clienteService.filtraAtivosPorNome(nome));
         }
         return new ModelAndView("pedido/listaClientes", "clientes", clienteService.listarClientesAtivos());
+    }
+
+    @RequestMapping(path = "/novo/{id}", method = RequestMethod.GET)
+    public ModelAndView viewExibe(@PathVariable("id") Long id) {
+        ModelAndView mv = new ModelAndView("pedido/novo");
+        mv.addObject("cliente", clienteService.buscarClientePorId(id));
+        mv.addObject("pedido", new PedidoDTO());
+        return mv;
     }
 }
